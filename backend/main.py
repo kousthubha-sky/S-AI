6e8493c462b02c08ca5a2fed5d915b6d0c774b8b
koustubha_sky_ <koustubhayadiyala36@gmail.com>
2025-11-09 19:23:57 +0530

    kuberns try

--- backend/main.py
+++ backend/main.py
@@ -1295,4 +1295,4 @@ async def fix_subscription_status(payload: dict = Depends(verify_token)):
 
 if __name__ == "__main__":
     # Start the FastAPI server
-    uvicorn.run("main:app", host="127.0.0.1", port=16830, reload=True)
+    uvicorn.run("main:app",host="0.0.0.0", port=8000, reload=True)
